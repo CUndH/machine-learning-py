f7b8045da3a2547d21539b347da1721611df50b5
CUndH <875021528@qq.com>
2019-12-24 16:53:31 +0800

    线性回归预测学习时长和分数

---
+++ student.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 数据预处理
+dataset = pd.read_csv('studentscores.csv')
+X = dataset.iloc[:, :1].values
+Y = dataset.iloc[:, 1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split( X, Y, test_size = 1/4, random_state = 0) 
+
+# 训练集使用简单回归模型进行训练
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor = regressor.fit(X_train, Y_train)
+
+# 预测结果
+Y_pred = regressor.predict(X_test)
+
+# 训练集结果可视化
+plt.scatter(X_train , Y_train, color = 'red')
+plt.plot(X_train , regressor.predict(X_train), color ='blue')
+plt.show()
